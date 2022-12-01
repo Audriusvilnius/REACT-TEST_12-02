e0cbbc7060f6ad2df3bf96000bd6b181f71f786e
audriusmacbook <audrius.ivko@gmail.com>
2022-12-01 13:36:35 +0200

    firstcom

---
+++ r1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="App-header">
+              
+            </div>
+        </div>
+    );
+}
+
+export default App;
